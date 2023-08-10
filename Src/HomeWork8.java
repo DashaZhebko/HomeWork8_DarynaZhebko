a1245562b3edbd9f30016d2b33c99c1581fe2a51
DashaZhebko <daryna.zhebko@gmail.com>
2023-08-10 21:14:49 +0300

    100 номерів без чисел 4 і 9

---
+++ Src/HomeWork8.java
@@ -0,0 +1,32 @@
+public class HomeWork8 {
+
+    public static boolean numberFilter(int num) {
+
+        while (num > 0) {
+            int result = num % 10;
+            if (result == 4 || result == 9) {
+                return true;
+            } else {
+                num = num / 10;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        int shuttleNum = 1;
+        int count = 0;
+        System.out.println("Shuttle numbers: ");
+        while (true) {
+            if (!HomeWork8.numberFilter(shuttleNum)) {
+                System.out.print(shuttleNum + ", ");
+                count++;
+            }
+            shuttleNum++;
+            if (count == 100) {
+                break;
+            }
+        }
+    }
+}
